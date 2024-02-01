addf18c7ff83fa3335aa2ca0677f79f71455a9f7
Bianca <bianca.manueno@gmail.com>
2024-02-01 21:19:45 +0100

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Bianca MANUENO on 01/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
